653058e79bef08eed73b94f37a9a127ea3c912d7
ebrianne <eldwan.brianne@desy.de>
2020-05-07 11:56:40 +0200

    Updated cmake folder

--- cmake/calice_default_cxx_flags.cmake
+++ cmake/calice_default_cxx_flags.cmake
@@ -2,22 +2,29 @@
 ## This runs checks if compilers support the flag and sets them, if they do
 ## this will create a humongous amount of warnings when compiling :)
 
+
 INCLUDE(CheckCXXCompilerFlag)
 
-SET(COMPILER_FLAGS -Wall -Wextra -Wshadow -Weffc++ -pedantic -Wno-long-long -Wuninitialized -fPIC )
+SET(COMPILER_FLAGS -Wall -Wextra -Wshadow -Weffc++ -pedantic -Wno-long-long -Wuninitialized -fPIC)
 
-IF( NOT APPLE )
+IF( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" )
  LIST( APPEND COMPILER_FLAGS -Wl,-no-undefined )
 ENDIF()
 
-
 MESSAGE( STATUS "FLAGS ${COMPILER_FLAGS}" )
 FOREACH( FLAG ${COMPILER_FLAGS} )
-  CHECK_CXX_COMPILER_FLAG( "${FLAG}" CXX_FLAG_WORKS_${FLAG} )
-  IF( ${CXX_FLAG_WORKS_${FLAG}} )
+
+  ## meed to replace the minus or plus signs from the variables, because it is passed
+  ## as a macro to g++ which causes a warning about no whitespace after macro
+  ## definition
+  STRING(REPLACE "-" "_" FLAG_WORD ${FLAG} )
+  STRING(REPLACE "+" "P" FLAG_WORD ${FLAG_WORD} )
+
+  CHECK_CXX_COMPILER_FLAG( "${FLAG}" CXX_FLAG_WORKS_${FLAG_WORD} )
+  IF( ${CXX_FLAG_WORKS_${FLAG_WORD}} )
     MESSAGE ( STATUS "Adding ${FLAG} to CXX_FLAGS" )
     ### We prepend the flag, so they are overwritten by whatever the user sets in his own configuration
-    SET ( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS} ")
+    SET ( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS}")
   ELSE()
     MESSAGE ( STATUS "NOT Adding ${FLAG} to CXX_FLAGS" )
   ENDIF()
@@ -26,12 +33,19 @@ ENDFOREACH()
 OPTION( USE_CXX11 "Use CXX Standard 2011" True )
 IF( USE_CXX11 )
   SET( FLAG "-std=c++11" )
-  CHECK_CXX_COMPILER_FLAG( ${FLAG} CXX_FLAG_WORKS_${FLAG} )
-  IF( ${CXX_FLAG_WORKS_${FLAG}} )
-    SET( CMAKE_CXX_FLAGS " ${FLAG} ${CMAKE_CXX_FLAGS}")
+  SET( FLAG_WORD "stdCXX11" )
+  CHECK_CXX_COMPILER_FLAG( ${FLAG} CXX_FLAG_WORKS_${FLAG_WORD} )
+  IF( ${CXX_FLAG_WORKS_${FLAG_WORD}} )
+    SET( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS}")
   ELSE()
     MESSAGE( FATAL_ERROR "Cannot add ${FLAG} to CMAKE_CXX_FLAGS, but c++11 was requested, check your compiler" )
   ENDIF()
 ELSE()
   MESSAGE( STATUS "NOT building with CXX11 standard" )
 ENDIF()
+
+IF( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 4.9 )
+  SET( CMAKE_CXX_FLAGS "-fdiagnostics-color=auto ${CMAKE_CXX_FLAGS}" )
+ENDIF()
+
+SET( CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS_RELWITHDEBINFO} ${CMAKE_CXX_FLAGS}")
